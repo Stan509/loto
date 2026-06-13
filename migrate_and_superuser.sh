30813bc3482875ca2bab741ebc974269f542bfd7
Stanley <admin@gaboomholding.com>
2026-06-13 02:14:38 -0400

    chore: remove database reset script after successful deployment

--- migrate_and_superuser.sh
+++ migrate_and_superuser.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-echo "Resetting database..."
-python reset_db_prod.py
 echo "Running database migrations..."
 python manage.py migrate --noinput
 echo "Running superuser creation script..."
